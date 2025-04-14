cf2cd6f57e0044f1d8eb84385d01f8cb0e81ff35
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-14 16:15:20 +0200

    add-for-snack1

--- main.js
+++ main.js
@@ -3,18 +3,32 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
-const bycicleRunning = [
+const bicycleRunning = [
   {
+    // i = 0
     name: "colnago",
-    weight: "30",
+    weight: 30,
   },
   {
+    // i = 1
     name: "triban",
-    weight: "27",
+    weight: 27,
   },
   {
     name: "rockrider",
-    weight: "23",
+    weight: 23,
   },
 ];
-console.log(bycicleRunning[2]);
+console.log(bicycleRunning[2]);
+
+// Oggetto che mi memorizzerà la bici con il peso minore
+let bicycleWeightMin = bicycleRunning[0];
+
+for (let i = 1; i < bicycleRunning.length; i++) {
+  const currentBicycleRunning = bicycleRunning[i];
+  if (currentBicycleRunning.weight < bicycleWeightMin.weight) {
+    bicycleWeightMin = currentBicycleRunning;
+  }
+}
+
+console.log(bicycleWeightMin);
