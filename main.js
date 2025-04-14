13350002f9327bc5a7e02c482b6c37712d34fdb1
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-14 15:38:24 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// #### Snack 1
+
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+const bycicleRunning = [
+  {
+    name: "colnago",
+    weight: "30",
+  },
+  {
+    name: "triban",
+    weight: "27",
+  },
+  {
+    name: "rockrider",
+    weight: "23",
+  },
+];
+console.log(bycicleRunning[2]);
